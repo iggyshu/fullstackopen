37e894d051cf3dc780a329c2c78a0ede286ac497
Igors Šutovs <iggy.shu@mykolab.com>
2021-12-29 08:22:13 +0100

    fmt src

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const Header = (props) => (
   <>
@@ -12,34 +12,34 @@ const Part = (props) => (
       {props.name} {props.exercises}
     </p>
   </div>
-)
+);
 
 const Content = (props) => (
   <div>
-    <Part name={props.part1} exercises={props.exercises1}/>
-    <Part name={props.part2} exercises={props.exercises2}/>
-    <Part name={props.part3} exercises={props.exercises3}/>
+    <Part name={props.part1} exercises={props.exercises1} />
+    <Part name={props.part2} exercises={props.exercises2} />
+    <Part name={props.part3} exercises={props.exercises3} />
   </div>
 );
 
 const Total = (props) => (
   <div>
-      <p>Number of exercises {props.total}</p>
+    <p>Number of exercises {props.total}</p>
   </div>
-)
+);
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
-      <Header course={course}/>
+      <Header course={course} />
       <Content
         part1={part1}
         part2={part2}
@@ -48,9 +48,9 @@ const App = () => {
         exercises2={exercises2}
         exercises3={exercises3}
       />
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
