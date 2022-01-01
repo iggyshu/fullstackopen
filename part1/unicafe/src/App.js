d87819882c2eb7f7a1a35078402a3c0942d10c8d
Igors Šutovs <iggy.shu@mykolab.com>
2022-01-01 14:21:17 +0100

    part1: ex 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,22 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+
+const Statistics = ({ good, neutral, bad, all, average, positive }) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <div>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+        <div>all {all}</div>
+        <div>average {average}</div>
+        <div>positive {positive} %</div>
+      </div>
+    </div>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const getTotal = () => {
     return good + neutral + bad;
-  }
+  };
 
   const getAverage = () => {
     return (good - bad) / getTotal();
-  }
+  };
 
   const getPositive = () => {
-    return good / getTotal() * 100;
-  }
+    return (good / getTotal()) * 100;
+  };
 
   return (
     <div>
@@ -26,17 +42,16 @@ const App = () => {
         <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
         <button onClick={() => setBad(bad + 1)}>bad</button>
       </div>
-      <h1>statistics</h1>
-      <div>
-        <div>good {good}</div>
-        <div>neutral {neutral}</div>
-        <div>bad {bad}</div>
-        <div>all {getTotal()}</div>
-        <div>average {getAverage()}</div>
-        <div>positive {getPositive()} %</div>
-      </div>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={getTotal()}
+        average={getAverage()}
+        positive={getPositive()}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
