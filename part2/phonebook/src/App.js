44d975ae96024dc5220c3b70dd4249933a3b8b9f
Igors Šutovs <iggy.shu@mykolab.com>
2022-01-04 20:30:41 +0100

    part2: ex 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,13 +3,17 @@ import React, { useState } from "react";
 const Number = ({ name }) => <div>{name}</div>;
 
 const App = () => {
-  const [persons, setPersons] = useState([{ id: 0, name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    setPersons(persons.concat([{ id: persons.length, name: newName }]));
-    setNewName("");
+    if (persons.find((item) => item.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat([{ name: newName }]));
+      setNewName("");
+    }
   };
 
   const handleNameChange = (event) => {
@@ -31,7 +35,7 @@ const App = () => {
       <div>debug: {newName}</div>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <Number name={person.name} key={person.id} />
+        <Number name={person.name} key={person.name} />
       ))}
     </div>
   );
