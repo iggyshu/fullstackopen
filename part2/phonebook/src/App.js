9519b20dd650278db8aa7673a8deae793568ce86
Igors Šutovs <iggy.shu@mykolab.com>
2022-01-05 07:24:09 +0100

    part2: ex 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -9,9 +9,14 @@ const Number = ({ name, number }) => (
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchParam, setSearchParam] = useState("");
+  const [matchedPersons, setMatchedPersons] = useState(persons);
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
@@ -19,8 +24,23 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
     } else {
       setPersons(persons.concat([{ name: newName, number: newNumber }]));
+      setMatchedPersons(persons.concat([{ name: newName, number: newNumber }]));
       setNewName("");
       setNewNumber("");
+      setSearchParam("");
+    }
+  };
+
+  const handleSearchParamChange = (event) => {
+    console.log(event.target.value);
+    setSearchParam(event.target.value);
+    if (event.target.value.length === 0) {
+      setMatchedPersons(persons.concat());
+    } else {
+      const search = event.target.value.toLowerCase();
+      setMatchedPersons(
+        persons.filter((person) => person.name.toLowerCase().includes(search))
+      );
     }
   };
 
@@ -37,6 +57,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with:{" "}
+        <input value={searchParam} onChange={handleSearchParamChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleFormSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -50,7 +75,7 @@ const App = () => {
       </form>
       <div>debug: {newName}</div>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {matchedPersons.map((person) => (
         <Number name={person.name} number={person.number} key={person.name} />
       ))}
     </div>
