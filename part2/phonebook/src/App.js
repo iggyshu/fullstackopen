0f9d5ee0e1d759780542952ff16edcb737a70ae2
Igors Šutovs <iggy.shu@mykolab.com>
2022-01-05 12:02:38 +0100

    part2: ex 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,6 +6,40 @@ const Number = ({ name, number }) => (
   </div>
 );
 
+const Persons = ({ persons }) => (
+  <div>
+    {persons.map((person) => (
+      <Number name={person.name} number={person.number} key={person.name} />
+    ))}
+  </div>
+);
+
+const Filter = ({ value, onValueChange }) => (
+  <div>
+    filter shown with: <input value={value} onChange={onValueChange} />
+  </div>
+);
+
+const PersonForm = ({
+  onFormSubmit,
+  newName,
+  onNameChange,
+  newNumber,
+  onNumberChange,
+}) => (
+  <form onSubmit={onFormSubmit}>
+    <div>
+      name: <input value={newName} onChange={onNameChange} />
+    </div>
+    <div>
+      number: <input value={newNumber} onChange={onNumberChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+);
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-1234567" },
@@ -32,7 +66,7 @@ const App = () => {
   };
 
   const handleSearchParamChange = (event) => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
     setSearchParam(event.target.value);
     if (event.target.value.length === 0) {
       setMatchedPersons(persons.concat());
@@ -45,39 +79,29 @@ const App = () => {
   };
 
   const handleNameChange = (event) => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
     setNewName(event.target.value);
   };
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
     setNewNumber(event.target.value);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with:{" "}
-        <input value={searchParam} onChange={handleSearchParamChange} />
-      </div>
-      <h2>add a new</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <div>debug: {newName}</div>
-      <h2>Numbers</h2>
-      {matchedPersons.map((person) => (
-        <Number name={person.name} number={person.number} key={person.name} />
-      ))}
+      <Filter value={searchParam} onValueChange={handleSearchParamChange} />
+      <h3>Add a new</h3>
+      <PersonForm
+        onFormSubmit={handleFormSubmit}
+        newName={newName}
+        newNumber={newNumber}
+        onNameChange={handleNameChange}
+        onNumberChange={handleNumberChange}
+      />
+      <h3>Numbers</h3>
+      <Persons persons={matchedPersons} />
     </div>
   );
 };
