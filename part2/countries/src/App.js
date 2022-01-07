f83f52b562145d055d83be78e484e5c22f192c48
Igors Šutovs <iggy.shu@mykolab.com>
2022-01-07 07:37:46 +0100

    part2: ex 2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const Countries = ({countries}) => (
+    <div>
+        {countries.map((country) => (
+          <div key={country.name.official}>{country.name.common}</div>
+        ))}
+    </div>
+);
+
+const Country = ({country}) => (
+  <div>
+    <h2>{country.name.common}</h2>
+    <div>capital {country.capital[0]}</div>
+    <div>population {country.population}</div>
+    <h3>Languages</h3>
+    <ul>
+      {Object.keys(country.languages).map((key) => (
+        <li key={key}>{country.languages[key]}</li>
+      ))}
+    </ul>
+    <div>
+      <img alt="country flag" src={country.flags.svg} width="256" />
+    </div>
+  </div>
+);
+
 const App = () => {
   const [searchParam, setSearchParam] = useState("");
   const [countries, setCountries] = useState([]);
@@ -17,6 +42,17 @@ const App = () => {
     }
   }, [searchParam]);
 
+  let Main;
+
+  if (countries.length === 1) {
+    // console.log(countries[0]);
+    Main = (<Country country={countries[0]} />);
+  } else if (countries.length < 10) {
+    Main = (<Countries countries={countries} />);
+  } else {
+    Main = (<div>Too many matches, specify another filter</div>);
+  }
+
   return (
     <div>
       <div>
@@ -26,11 +62,7 @@ const App = () => {
           onChange={(event) => setSearchParam(event.target.value)}
         />
       </div>
-      <div>
-        {countries.map((country) => (
-          <div key={country.name.official}>{country.name.common}</div>
-        ))}
-      </div>
+      {Main}
     </div>
   );
 };
