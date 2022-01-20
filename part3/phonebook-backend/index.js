49018dd2616e5eceac34855b2188c77b79e7ff79
Igors Šutovs <iggy.shu@mykolab.com>
2022-01-20 07:19:58 +0100

    use express.json

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 let persons = [
   {
